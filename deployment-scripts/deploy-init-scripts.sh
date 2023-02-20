d27f5c71f4fba6eb801c801024f40121ced69227
ayodeji-AA <79341494+ayodeji-AA@users.noreply.github.com>
2023-02-20 17:15:50 +0000

    formatted bash script

--- deployment-scripts/deploy-init-scripts.sh
+++ deployment-scripts/deploy-init-scripts.sh
@@ -18,31 +18,33 @@ initScriptsPath="dbfs:/init-scripts"
 # Get access tokens for Databricks API
 ######################################################################################
 
-accessToken=$(curl -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
+accessToken=$(
+  curl -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
   -F resource=$azure_databricks_resource_id \
   -F client_id=$client_id \
   -F grant_type=client_credentials \
-  -F client_secret=$client_secret | jq .access_token --raw-output) 
+  -F client_secret=$client_secret | jq .access_token --raw-output
+)
 
-managementToken=$(curl -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
+managementToken=$(
+  curl -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
   -F resource=https://management.core.windows.net/ \
   -F client_id=$client_id \
   -F grant_type=client_credentials \
-  -F client_secret=$client_secret | jq .access_token --raw-output) 
-
+  -F client_secret=$client_secret | jq .access_token --raw-output
+)
 
 ######################################################################################
 # Get Databricks workspace URL (e.g. adb-5946405904802522.2.azuredatabricks.net)
 ######################################################################################
-workspaceUrl=$(curl -X GET \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $managementToken" \
-        https://management.azure.com/subscriptions/$subscription_id/resourcegroups/$resourceGroup/providers/Microsoft.Databricks/workspaces/$workspaceName?api-version=2018-04-01 \
-        | jq .properties.workspaceUrl --raw-output)
+workspaceUrl=$(
+  curl -X GET \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $managementToken" \
+  https://management.azure.com/subscriptions/$subscription_id/resourcegroups/$resourceGroup/providers/Microsoft.Databricks/workspaces/$workspaceName?api-version=2018-04-01 | jq .properties.workspaceUrl --raw-output
+)
 
 echo "Databricks workspaceUrl: $workspaceUrl"
-
-
 
 ######################################################################################
 # Create directory for Init Scripts
@@ -52,11 +54,11 @@ JSON="{ \"path\" : \"$initScriptsPath\" }"
 echo "curl https://$workspaceUrl/api/2.0/dbfs/mkdirs -d $JSON"
 
 curl -X POST https://$workspaceUrl/api/2.0/dbfs/mkdirs \
-    -H "Authorization:Bearer $accessToken" \
-    -H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
-    -H "Content-Type: application/json" \
-    --data "$JSON"
+-H "Authorization:Bearer $accessToken" \
+-H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
+-H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
+-H "Content-Type: application/json" \
+--data "$JSON"
 
 ######################################################################################
 # List Directories (just so we can see if it is created)
@@ -66,11 +68,11 @@ JSON="{ \"path\" : \"dbfs:/\" }"
 echo "curl https://$workspaceUrl/api/2.0/dbfs/list -d $JSON"
 
 curl -X GET https://$workspaceUrl/api/2.0/dbfs/list \
-    -H "Authorization:Bearer $accessToken" \
-    -H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
-    -H "Content-Type: application/json" \
-    --data "$JSON"
+-H "Authorization:Bearer $accessToken" \
+-H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
+-H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
+-H "Content-Type: application/json" \
+--data "$JSON"
 
 ######################################################################################
 # Upload Init Scripts
@@ -79,21 +81,21 @@ replaceSource="./"
 replaceDest=""
 
 find . -type f -name "*" -print0 | while IFS= read -r -d '' file; do
-    echo "Processing file: $file"
-    filename=${file//$replaceSource/$replaceDest}
-    echo "New filename: $filename"
+  echo "Processing file: $file"
+  filename=${file//$replaceSource/$replaceDest}
+  echo "New filename: $filename"
 
-    echo "curl -F path=$filename -F content=@$filename https://$workspaceUrl/api/2.0/dbfs/put"
+  echo "curl -F path=$filename -F content=@$filename https://$workspaceUrl/api/2.0/dbfs/put"
 
-    curl -n https://$workspaceUrl/api/2.0/dbfs/put \
-        -H "Authorization:Bearer $accessToken" \
-        -H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
-        -H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
-        -F overwrite=true \
-        -F path="$initScriptsPath/$filename" \
-        -F content=@"$filename"       
+  curl -n https://$workspaceUrl/api/2.0/dbfs/put \
+  -H "Authorization:Bearer $accessToken" \
+  -H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
+  -H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
+  -F overwrite=true \
+  -F path="$initScriptsPath/$filename" \
+  -F content=@"$filename"
 
-    echo ""
+  echo ""
 
 done
 
@@ -105,10 +107,10 @@ JSON="{ \"path\" : \"$initScriptsPath\" }"
 echo "curl https://$workspaceUrl/api/2.0/dbfs/list -d $JSON"
 
 curl -X GET https://$workspaceUrl/api/2.0/dbfs/list \
-        -H "Authorization:Bearer $accessToken" \
-        -H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
-        -H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
-        -H "Content-Type: application/json" \
-        --data "$JSON"
+-H "Authorization:Bearer $accessToken" \
+-H "X-Databricks-Azure-SP-Management-Token: $managementToken" \
+-H "X-Databricks-Azure-Workspace-Resource-Id: $resourceId" \
+-H "Content-Type: application/json" \
+--data "$JSON"
 
 echo ""
